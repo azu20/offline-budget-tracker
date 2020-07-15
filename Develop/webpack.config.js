fc668669d1cd5139dc71622806eb01480282bfc5
Azucena Herrera <azucenaphx@gmail.com>
2020-07-14 19:43:58 -0700

    webpacks

---
+++ Develop/webpack.config.js
@@ -0,0 +1,28 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+
+const config = {
+  entry: "./public/assets/js/app.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "production",
+  plugins: [
+    new WebpackPwaManifest({
+      // the name of the generated manifest file
+      filename: "manifest.json",
+
+      // we aren't using webpack to generate our html so we
+      // set inject to false
+      inject: false,
+
+      // set fingerprints to `false` to make the names of the generated
+      // files predictable making it easier to refer to them in our code
+      fingerprints: false,
+     
+    })
+  ]
+};
+
+module.exports = config;
